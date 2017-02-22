f7f4b9f19fed6c4880f98e36ea9e0a2e2b44b5a8
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-22 19:21:53 +0000

    Nfkd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-	var mycontact=navigator.contacts;
+
+	function onSuccess(contacts) {
+		for (var i = 0; i < contacts.length; i++) {
+			for (var j = 0; j < contacts[i].organizations.length; j++) {
+				alert("Pref: " + contacts[i].organizations[j].pref + "\n" + "Type: " + contacts[i].organizations[j].type + "\n" + "Name: " + contacts[i].organizations[j].name + "\n" + "Department: " + contacts[i].organizations[j].department + "\n" + "Title: " + contacts[i].organizations[j].title);
+			}
+		}
+	};
+
+	function onError(contactError) {
+		alert('onError!');
+	};
+
+	var options = new ContactFindOptions();
+	options.filter = "";
+	options.multiple = true;
+	filter = ["displayName", "organizations"];
+	var mycontact = navigator.contacts.find(filter, onSuccess, onError, options);
 	alert(mycontact);
-	// console.log(navigator.contacts);
 }
