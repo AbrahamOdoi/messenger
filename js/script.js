1b52ccb628e6c2e6e4a6761e763cbd549d22d14c
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-22 20:13:26 +0000

    Dfs

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ alert('yes');
 	navigator.contactsPhoneNumbers.list(function(contacts) {
 		alert(contacts.length + ' contacts found');
 		console.log(contacts.length + ' contacts found');
-		for (var i = 0; i < contacts.length; i++) {
-			console.log(contacts[i].id + " - " + contacts[i].displayName);
-			for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-				var phone = contacts[i].phoneNumbers[j];
-				alert("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
-				console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
-			}
-		}
+		// for (var i = 0; i < contacts.length; i++) {
+			// console.log(contacts[i].id + " - " + contacts[i].displayName);
+			// for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+				// var phone = contacts[i].phoneNumbers[j];
+				// alert("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+				// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+			// }
+		// }
 	}, function(error) {
 		// console.error(error);
 		alert(error);
