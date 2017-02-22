d1d9bef13c433f4d30242a29da515b13035cb95e
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-22 20:06:36 +0000

    fd
    ds

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 alert('yes');
 	navigator.contactsPhoneNumbers.list(function(contacts) {
+		alert(contacts.length + ' contacts found');
 		console.log(contacts.length + ' contacts found');
 		for (var i = 0; i < contacts.length; i++) {
 			console.log(contacts[i].id + " - " + contacts[i].displayName);
