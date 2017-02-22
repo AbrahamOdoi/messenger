ce389d8863e2f75c31d0f5e20c8bdaf66e987c4e
Abraham Odoi <abrahamodoi2002@gmail.com>
2017-02-22 20:17:28 +0000

    mds

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-alert('yes');
-	navigator.contactsPhoneNumbers.list(function(contacts) {
-		alert(contacts.length + ' contacts found');
-		console.log(contacts.length + ' contacts found');
-		// for (var i = 0; i < contacts.length; i++) {
+	alert('yes');
+
+	$("#mycon").click(function() {
+		alert('byes');
+		navigator.contactsPhoneNumbers.list(function(contacts) {
+			alert(contacts.length + ' contacts found');
+			console.log(contacts.length + ' contacts found');
+			// for (var i = 0; i < contacts.length; i++) {
 			// console.log(contacts[i].id + " - " + contacts[i].displayName);
 			// for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-				// var phone = contacts[i].phoneNumbers[j];
-				// alert("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
-				// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+			// var phone = contacts[i].phoneNumbers[j];
+			// alert("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
+			// console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber + ")");
 			// }
-		// }
-	}, function(error) {
-		// console.error(error);
-		alert(error);
+			// }
+		}, function(error) {
+			// console.error(error);
+			alert(error);
+		});
 	});
 	alert('yess');
 }
